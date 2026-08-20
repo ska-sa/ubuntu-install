55af94885169d37061352838525489cf707ad6c3
Jan Groenewald <jgroenewald@sarao.ac.za>
2026-08-20 11:38:30 +0200

    Fix make-iso and ignore ISOs

--- make-sarao-iso.sh
+++ make-sarao-iso.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
 
-# Get default boot options
-#xorriso -indev ~/Downloads/ubuntu-26.04-desktop-amd64.iso -report_el_torito as_mkisofs > ~/ubuntu-install/boot_opts.txt
+set -euo pipefail
 
-# Mount and copy Ubuntu installer
-sudo rm -rf /tmp/iso-orig /tmp/iso-new
-mkdir -p /tmp/iso-orig /tmp/iso-new
-sudo mount -o loop ~/Downloads/ubuntu-26.04-desktop-amd64.iso /tmp/iso-orig
-sudo rsync -a /tmp/iso-orig/ /tmp/iso-new/
-sudo umount /tmp/iso-orig
+ORIGINAL_ISO="$HOME/Downloads/ubuntu-26.04-desktop-amd64.iso"
+OUTPUT_ISO="$HOME/ubuntu-install/sarao-ubuntu-26.iso"
+GRUB_CFG="/tmp/sarao-grub.cfg"
+SEED_URL="https://raw.githubusercontent.com/ska-sa/ubuntu-install/main/"
 
-# Edit copy of installer
-sudo chown -R "$USER:$USER" /tmp/iso-new
-chmod +w /tmp/iso-new/boot/grub
-chmod +w /tmp/iso-new/boot/grub/grub.cfg
-sed -i 's#quiet splash#quiet splash autoinstall ds=nocloud-net;s=https://raw.githubusercontent.com/ska-sa/ubuntu-install/main/#' /tmp/iso-new/boot/grub/grub.cfg
-sed -i 's/set timeout=[0-9]*/set timeout=3/' /tmp/iso-new/boot/grub/grub.cfg
+rm -f "$OUTPUT_ISO" "$GRUB_CFG"
 
-# Create new ISO
-cd /tmp/iso-new
-xorriso -as mkisofs -r -V "SARAO-Ubuntu-26"  \
-  --modification-date='2026082007585100' \
-  --grub2-mbr --interval:local_fs:0s-15s:zero_mbrpt,zero_gpt:'/home/jan/Downloads/ubuntu-26.04-desktop-amd64.iso' \
-  --protective-msdos-label \
-  -partition_cyl_align off \
-  -partition_offset 16 \
-  --mbr-force-bootable \
-  -append_partition 2 28732ac11ff8d211ba4b00a0c93ec93b --interval:local_fs:12721412d-12731707d::'/home/jan/Downloads/ubuntu-26.04-desktop-amd64.iso' \
-  -appended_part_as_gpt \
-  -iso_mbr_part_type a2a0d0ebe5b9334487c068b6b72699c7 \
-  -c '/boot.catalog' \
-  -e '--interval:appended_partition_2_start_3180353s_size_10296d:all::' \
-  -no-emul-boot \
-  -boot-load-size 10296 \
-  -J -joliet-long \
-  -o ~/ubuntu-install/sarao-ubuntu-26.iso \
-  .
+# Extract the stock Ubuntu GRUB configuration.
+xorriso -osirrox on \
+    -indev "$ORIGINAL_ISO" \
+    -extract /boot/grub/grub.cfg "$GRUB_CFG"
+
+# Enable unattended installation from the NoCloud seed URL.
+# The \\; produces a literal \; in grub.cfg, so GRUB passes
+# the semicolon to the kernel instead of treating it as a command separator.
+sed -i \
+    "s#quiet splash#quiet splash autoinstall ds=nocloud-net\\\\;s=$SEED_URL#" \
+    "$GRUB_CFG"
+
+# Reduce the GRUB menu timeout.
+sed -i \
+    's/set timeout=[0-9]*/set timeout=3/' \
+    "$GRUB_CFG"
+
+# Rebuild from the original ISO, replacing ONLY grub.cfg.
+xorriso \
+    -indev "$ORIGINAL_ISO" \
+    -outdev "$OUTPUT_ISO" \
+    -map "$GRUB_CFG" /boot/grub/grub.cfg \
+    -boot_image any replay \
+    -commit
+
+echo "Created $OUTPUT_ISO"
